22760613bff61dec2be1d38e2124f94d843fc164
lizheng <179272789@qq.com>
2018-11-28 21:20:53 +0800

    一键生成固定男友

--- src/main/java/com/lizheng/web/controller/FirstController.java
+++ src/main/java/com/lizheng/web/controller/FirstController.java
@@ -1,13 +1,33 @@
 package com.lizheng.web.controller;
 
+import com.lizheng.web.model.Boyfriend;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
 
-@RestController
+@Controller
 public class FirstController {
 
-    @GetMapping("/test")
-    public String test() {
-        return "2019一定发财一定变瘦一定脱单啦啦啦！";
+    @GetMapping("/yeah")
+    public String test(Model model) {
+
+        Boyfriend boyfriend = new Boyfriend();
+//        boyfriend.setName("赵本山");
+        model.addAttribute("boyfriend",boyfriend);
+        return "index";
+    }
+
+    @PostMapping("/boy")
+    public String boy(Model model) {
+        Boyfriend boyfriend = new Boyfriend();
+        boyfriend.setName("赵本山");
+        boyfriend.setHeight(175);
+        boyfriend.setAge(61);
+        boyfriend.setWeight(70);
+        boyfriend.setCharacter("逗");
+        boyfriend.setLink("https://baike.baidu.com/pic/%E8%B5%B5%E6%9C%AC%E5%B1%B1/131652/0/48540923dd54564e078bd8d5b6de9c82d0584f6b?fr=lemma&ct=single#aid=0&pic=48540923dd54564e078bd8d5b6de9c82d0584f6b");
+        model.addAttribute("boyfriend",boyfriend);
+        return "index";
     }
 }
---
+++ src/main/java/com/lizheng/web/model/Boyfriend.java
@@ -0,0 +1,13 @@
+package com.lizheng.web.model;
+
+import lombok.Data;
+
+@Data
+public class Boyfriend {
+    private String name;
+    private Integer height;
+    private Integer weight;
+    private String character;
+    private Integer age;
+    private String link;
+}
